f38165da87d20cc6083f6ec1f1ec8e807a03cf38
LowTechTurtle <dongquocthang1235@gmail.com>
2024-09-08 17:17:27 +0700

    update

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -125,37 +125,46 @@ class TestAccountService(TestCase):
 
     # ADD YOUR TEST CASES HERE ...
 
-    def test_read_an_account(self):
+    def test_get_account(self):
+        """It should Read a single Account"""
         account = self._create_accounts(1)[0]
-        resp = self.client.post(f"{BASE_URL}/{account.id}", json=account.serialize(),
-            content_type="application/json")
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
-        self.assertEqual(account.name, data["name"])
+        self.assertEqual(data["name"], account.name)
 
-    def test_list_account(self):
-        accounts = self._create_accounts(10)
-        resp = self.client.post(f"{BASE_URL}")
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
-        self.assertEqual(len(accounts), len(data))
+        self.assertEqual(len(data), 5)
 
     def test_update_account(self):
-        account = self._create_accounts(1)[0]
-        resp = self.cilent.post(f"{BASE_URL}", json=account.serialize(), content_type="application/json")
+        """It should Update an existing Account"""
+        # create an Account to update
+        test_account = AccountFactory()
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
         self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
-        
-        new_acc = resp.get_json()
-        new_acc["name"] = "banana"
-        resp = self.client.put(f"{BASE_URL}/{new_acc["id"]}", json=new_acc)
+
+        # update the account
+        new_account = resp.get_json()
+        new_account["name"] = "Something Known"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        self.assertEqual(resp.get_json()["name"], "banana")
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Something Known")
     
     def test_delete_account(self):
+        """It should Delete an Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
